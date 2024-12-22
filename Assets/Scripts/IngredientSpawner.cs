e76a0224de2bf27371a5c9aedacd494b3b3a7098
andreeav19 <andra_368@yahoo.com>
2024-12-22 19:15:52 +0200

    added spawn point for ingredients

---
+++ Assets/Scripts/IngredientSpawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngredientSpawner : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> goodIngredients;
+    [SerializeField] private List<GameObject> badIngredients;
+    
+    [Range(0f, 100f)]
+    [SerializeField] private float goodIngredientProbability = 75f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateIngredient();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateIngredient()
+    {
+        bool isGoodIngredient = GenerateIngredientType();
+
+        if (isGoodIngredient && goodIngredients.Count != 0)
+            InstantiateIngredient(goodIngredients);
+        else if (!isGoodIngredient && badIngredients.Count != 0)
+            InstantiateIngredient(badIngredients);
+    }
+    
+    // Returns true for good ingredients and false for bad ingredients
+    bool GenerateIngredientType()
+    {
+        return UnityEngine.Random.Range(0, 101) < goodIngredientProbability;
+    }
+
+    void InstantiateIngredient(List<GameObject> ingredients)
+    {
+        var randomIndex = UnityEngine.Random.Range(0, ingredients.Count);
+        GameObject.Instantiate(ingredients[randomIndex],
+            gameObject.transform.position,
+            Quaternion.identity,
+            gameObject.transform
+        );
+    }
+}
